ebe100d6cec6938540d297343cd6d13fb4f1e3ba
Nishahtc <nishamodi254@email.com>
2024-08-26 13:30:47 +0530

    test

---
+++ Augest_26/shape.py
@@ -0,0 +1,14 @@
+"""import numpy as np
+arr=np.array([1,2,3,4,5,6])
+print(np.shape(arr))
+arr1=np.array([1,2,3,4,5])
+arr2=np.array([10,20,30,40,50])
+print(np.size(arr1))
+print(np.ndim(arr2))"""
+import numpy as np
+arr=np.array([1,2,3,4,5,6])
+print(arr)
+new=arr.astype(float)
+print(new)
+a=len(arr)
+print(a)
